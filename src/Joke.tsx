8f360bc5725147d828a7377588e2ff221a605440
MartinNuc <martin.nuc@productboard.com>
2026-02-25 11:47:32 +0100

    request error handling

--- src/Joke.tsx
+++ src/Joke.tsx
@@ -6,20 +6,33 @@ type JokeResponse = {
 
 export const Joke = () => {
     const [joke, setJoke] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         // eslint-disable-next-line react-hooks/set-state-in-effect, react-hooks/immutability
         void fetchNext();
-    }, []);    
-    
-    const fetchNext = async () => {
-        const response = await fetch('https://api.chucknorris.io/jokes/random')
-        const body: JokeResponse = await response.json();
+    }, []);
 
-        setJoke(body.value);
+    const fetchNext = async () => {
+        try {
+            setIsLoading(true);
+            const response = await fetch('https://api.chucknorris.io/jokes/random')
+            const body: JokeResponse = await response.json();
+
+            setJoke(body.value);
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                setError(e);
+            }
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return <div>
+        {isLoading && <span>🕰️</span>}
+        {error && <p>{error.message}</p>}
         <p>{joke}</p>
         <button onClick={fetchNext}>fetch next</button>
     </div>
